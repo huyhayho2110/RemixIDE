61e1d42c444d5668a35dd96693903d351a9ac107
huyhayho2110 <huytd.ba12-085@st.usth.edu.vn>
2024-07-22 01:14:28 +0700

    1

--- contracts/Ex4.VariablesScope.sol
+++ contracts/Ex4.VariablesScope.sol
@@ -1,2 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+contract EX4 {
+    uint public publicData = 100; // Public variable
+    uint internal internalData = 200; // Internal variable
+    uint private privateData = 300; // Private variable
+
+    //public function to set publicData
+    function setpublicData(uint _value) public {
+        publicData = _value;
+    }
+
+    //public function to read internalData
+    function getinternalData() public view returns (uint){
+        return internalData;
+    }
+}
